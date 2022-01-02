6840429d863d97da13295a4dcdd7c3b23f99dc21
Jing LIU <jing.liu@usd.edu>
2022-01-02 02:23:03 -0500

    added accumulated sum to records

--- view.C
+++ view.C
@@ -1,18 +1,20 @@
-void view(const char* fi="output.root")
+void view(const char* fi="input.root")
 {
 	TTreeViewer *viewer = new TTreeViewer;
 	viewer->SetScanRedirect(kTRUE); // avoid involving terminal output
 
 	TFile *f = new TFile(fi);
  	TTree *t;
-	for (int ch=0; ch<16; ch++) {
-		t = (TTree*) f->Get(Form("t%d",ch));
-		viewer->AppendTree(t);
+	for (int m=0; m<21; m++) {
+		for (int c=0; c<16; c++) {
+			t = (TTree*) f->Get(Form("t%d",m*16+c));
+			if (t) viewer->AppendTree(t);
+		}
 	}
 
 	// items (0:x, 1:y, 2:z, 3:cut, 4:scan, users must start at 5)
 	viewer->ExpressionItem(5)->SetExpression("h", "~height", kFALSE);
-	viewer->ExpressionItem(6)->SetExpression("th", "~peak position", kFALSE);
+	viewer->ExpressionItem(6)->SetExpression("ih", "~peak position", kFALSE);
 	viewer->ExpressionItem(7)->SetExpression("n", "~number of samples", kFALSE);
 
 	// records (for better control)
@@ -22,14 +24,29 @@ void view(const char* fi="output.root")
 	TTVRecord* record = session->AddRecord(kTRUE);
 	record = session->AddRecord(kTRUE);
 	session->SetRecordName("Waveforms");
-	record->fX        = "Iteration$";
-	record->fY        = "adc";
+	record->fX        = "t";
+	record->fY        = "s";
 	record->fZ        = "";
 	record->fCut      = "Entry$<10";
-	record->fXAlias   = "~Time";
+	record->fXAlias   = "~Time [ns]";
 	record->fYAlias   = "~ADC unit";
 	record->fZAlias   = "-empty-";
 	record->fCutAlias = "~10evts";
+	record->fOption   = "l";
+	record->fScanRedirected = kFALSE;
+	record->fCutEnabled = kTRUE;
+
+	// 2nd record
+	record = session->AddRecord(kTRUE);
+	session->SetRecordName("Accumulated Sums");
+	record->fX        = "Iteration$";
+	record->fY        = "sum";
+	record->fZ        = "";
+	record->fCut      = "Entry$<50";
+	record->fXAlias   = "~Accumulator";
+	record->fYAlias   = "~ADC unit";
+	record->fZAlias   = "-empty-";
+	record->fCutAlias = "~50evts";
 	record->fOption   = "l";
 	record->fScanRedirected = kFALSE;
 	record->fCutEnabled = kTRUE;
