3d89da97d5096b2c6cc78126378f62f44d2c729a
Jing Liu <jintonic@physino.xyz>
2023-03-15 23:48:36 -0400

    process only july 29 data

--- TUNL/CsI/QF/integrate.sh
+++ TUNL/CsI/QF/integrate.sh
@@ -3,15 +3,21 @@ echo "check ROOT files in current folder..."
 njobs=`ls -1 SIS*Raw_*_*.root 2>/dev/null | wc -l`
 if [ $njobs -le 0 ]; then echo "no ROOT file found. Quit."; exit; fi
 
-echo "process $njobs ROOT files..."
-for file in `ls -1 $PWD/SIS*Raw_*_*.root`; do
+echo "process $njobs ROOT files on July 29, 2022 ..."
+for file in `ls -1 $PWD/SIS*Raw_20220729*_*.root`; do
   name=$(basename -- $file) # remove path from file name
   out=Integrated${name#SIS3316Raw};
   if [ -f $out ]; then continue; fi # skip processed files
   
   log=${name%root}log; err=${name%root}err; script=${name%root}sh
   echo "#!/bin/sh" > $script
-  echo "root -b -q $PWD/integrate.C'(\"$file\")'" >> $script
+  if [[ $name == *130801* ]]; then
+    echo "root -b -q $PWD/integrate.C'(\"$file\", 17935)'" >> $script
+  elif [[ $name == *182748* ]]; then
+    echo "root -b -q $PWD/integrate.C'(\"$file\", 17748)'" >> $script
+  else
+    echo "root -b -q $PWD/integrate.C'(\"$file\")'" >> $script
+  fi
   number=${name##*_}; number=${number%.root} # get number from file name
   # man qsub. -V: copy ENV to node; err & output must be separated at hcdata
   qsub -V -N int$number -o $log -e $err $script
