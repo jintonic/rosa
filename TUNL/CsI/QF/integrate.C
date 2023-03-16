18e5b8779b1fecbc9c9970509423434947075fc6
Jing Liu <jintonic@physino.xyz>
2023-03-15 23:47:15 -0400

    corrected overshoot

--- TUNL/CsI/QF/integrate.C
+++ TUNL/CsI/QF/integrate.C
@@ -1,10 +1,12 @@
 // integrate a waveform after its baseline aligned to zero
-void integrate(const char* run="SIS3316Raw_20220727233254_1.root")
+void integrate(const char* run="SIS3316Raw_20220729182748_9.root",
+		double tau=18000 /* RC constant for overshooting correction */)
 {
 	int i, n, m, np, bd, row, entry; bool is;
-	float a, ah, b, db, f, h, tt, p, dp, dt, en, r, hm, lm, ht, lt;
+	float a, ah, b, db, f, h, tt, p, dp, pb, dt, hm, lm, ht, lt, e2, e3, e4, e5;
 	float s[1024], t[1024], v[4096], ns[4096], sb[4096], tb[4096];
 	float pa[100], pt[100], ph[100], bgn[100], end[100];
+	float vc[4096]={0};
 
 	TFile *input = new TFile(run);
 	const int nc=14; // number of channels enabled
@@ -32,16 +34,20 @@ void integrate(const char* run="SIS3316Raw_20220727233254_1.root")
 
 	to->Branch("m",&m,"m/I"); // number of samples in CsI waveform
 	to->Branch("v",v,"v[m]/F"); // CsI waveform sample value in unit of ADC
+	to->Branch("vc",vc,"vc[m]/F"); // CsI corrected waveform in unit of ADC
 	to->Branch("ns",ns,"ns[m]/F"); // CsI waveform index in unit of ns
-	to->Branch("p",&p,"p/F"); // pedestal of CsI averaged over 300 samples
+	to->Branch("p",&p,"p/F"); // pedestal of CsI averaged over 300 samples before integral
+	to->Branch("pb",&pb,"pb/F"); // pedestal of CsI averaged over 300 samples after integral
 	to->Branch("dp",&dp,"dp/F"); // RMS of pedestal of CsI waveform
 	to->Branch("np",&np,"np/I"); // number of pulses in a CsI waveform
-	to->Branch("en",&en,"en/F"); // integral in [300,1000] in CsI waveform
+	to->Branch("e2",&e2,"e2/F"); // integral in [1200,2000] ns in CsI waveform
+	to->Branch("e3",&e3,"e3/F"); // integral in [1200,3000] ns in CsI waveform
+	to->Branch("e4",&e4,"e4/F"); // integral in [1200,4000] ns in CsI waveform
+	to->Branch("e5",&e5,"e5/F"); // integral in [1200,5000] ns in CsI waveform
 	to->Branch("hm",&hm,"hm/F"); // max height in [300,1000)
 	to->Branch("lm",&lm,"lm/F"); // lowest point in [300,1000)
 	to->Branch("ht",&ht,"ht/F"); // time of max height in [300,1000)
 	to->Branch("lt",&lt,"lt/F"); // time of lowest point in [300,1000)
-	to->Branch("r",&r,"r/F"); // integral in [300,400] / en
 	to->Branch("pa",pa,"pa[np]/F"); // pulse area
 	to->Branch("ph",ph,"ph[np]/F"); // pulse height
 	to->Branch("pt",pt,"pt[np]/F"); // pulse time (above threshold)
@@ -140,14 +146,20 @@ void integrate(const char* run="SIS3316Raw_20220727233254_1.root")
 		tt*=4; // convert to ns
 		if (tt<290 || f>0.6 || f<0.3 || h/a>0.4) continue;
 
-		en=0; r=0; hm=0; lm=9999; ht=-10; lt=-10;
-		for (int k=300; k<1000; k++) {
-			en+=v[k]; // calculate En in CsI
-			if (k<400) r+=v[k];
-			if (hm<v[k]) { ht=k; hm=v[k]; }
-			if (lm>v[k]) { lt=k; lm=v[k]; }
+		e2=0; hm=0; lm=9999; ht=-10; lt=-10; e3=0; e4=0; e5=0;
+		// correct overshoot
+		for (int k=1; k<m; k++) vc[k]=(tau+4)/tau*v[k]-v[k-1]+vc[k-1];
+		// calculate baseline after integration
+		for (int k=1250; k<1250+300; k++)	pb+=vc[k]; pb/=300;
+
+		for (int k=300; k<500; k++) e2+=vc[k];
+		for (int k=300; k<750; k++) e3+=vc[k];
+		for (int k=300; k<1000; k++) e4+=vc[k];
+		for (int k=300; k<1250; k++) {
+			e5+=vc[k]; // calculate corrected e5 in CsI
+			if (hm<vc[k]) { ht=k; hm=vc[k]; }
+			if (lm>vc[k]) { lt=k; lm=vc[k]; }
 		}
-		r/=en;
 
 		// process BPM waveforms
 		ti[13]->GetEntry(entry);
