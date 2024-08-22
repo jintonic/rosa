c78ac1204355a1c7c60363cc7eed133989450f69
Jing Liu <jintonic@physino.xyz>
2024-08-22 00:16:18 -0400

    updated

--- TUNL/CsI/n2MeV/PT405/40K/1kOhm/shrink.C
+++ TUNL/CsI/n2MeV/PT405/40K/1kOhm/shrink.C
@@ -1,67 +1,49 @@
-{ // shrink root file size by applying stronger cuts
-	TChain *told = new TChain("t");
-	for (int i=1; i<75; i++) told->Add(Form("Integrated_20220729182748_%d.root",i));
-	
-	told->SetBranchStatus("*",0);
-	told->SetBranchStatus("m",1);
-	told->SetBranchStatus("pb",1);
-	told->SetBranchStatus("e2",1);
-	told->SetBranchStatus("e3",1);
-	told->SetBranchStatus("e4",1);
-	told->SetBranchStatus("e5",1);
-	told->SetBranchStatus("hm",1);
-	told->SetBranchStatus("ht",1);
-	told->SetBranchStatus("bd",1);
-	told->SetBranchStatus("ns",1);
-	told->SetBranchStatus("vc",1);
-	told->SetBranchStatus("dt",1);
-	told->SetBranchStatus("pt",1);
+{ // shrink file size with stronger cuts
+	TChain c("t");
+	c.Add("Integrated_20240802154030.root");
+	c.Add("Integrated_20240802164141.root");
+	c.Add("Integrated_20240802184149.root");
+	c.Add("Integrated_20240802205121.root");
 
-	TFile *output = new TFile("shrinked.root","recreate");
-	TTree *t = told->CloneTree(0);
+	int m, bd; float f, dp, dt, e, e3, e4, e5, rms; float v[5000], ns[5000];
 
-	int m;
-	float  ht, hm; 
-	float vc[4096];	
-	told->SetBranchAddress("m",&m); // number of samples
-	told->SetBranchAddress("ht",&ht);  
-	told->SetBranchAddress("hm",&hm);
-	told->SetBranchAddress("vc",vc);
+	c.SetBranchStatus("*",0);
+	c.SetBranchAddress("m",&m);
+	c.SetBranchAddress("f",&f);  
+	c.SetBranchAddress("e",&e);
+	c.SetBranchAddress("v",v);
+	c.SetBranchAddress("ns",ns);
+	c.SetBranchAddress("dp",&dp);
+	c.SetBranchAddress("bd",&bd);
+	c.SetBranchAddress("dt",&dt);
 
-	float pc, dpc, dwi, pbc;
+	TFile output("shrinked.root","recreate");
+	TTree *t = c.CloneTree(0);
+	t->Branch("rms",&rms,"rms/F");   // RMS of CsI waveform in [1600,3000] ns
+	t->Branch("e3",&e3,"e3/F"); // integral of CsI waveform in [1600,3000] ns
+	t->Branch("e4",&e4,"e4/F"); // integral of CsI waveform in [1600,4000] ns
+	t->Branch("e5",&e5,"e5/F"); // integral of CsI waveform in [1600,5000] ns
 
-	t->Branch("pc",&pc,"pc/F"); // pedestal of CsI averaged over 300 samples before integral after correcting the overshoot
-	t->Branch("pbc",&pbc,"pbc/F"); // pedestal of CsI averaged over 300 samples after integral after correcting the overshoot 
-	t->Branch("dpc",&dpc,"dpc/F"); // RMS of pedestal of CsI waveform after correcting the overshoot
-	t->Branch("dwi",&dwi,"dwi/F"); // RMS of integral range of CsI waveform after correcting the overshoot
+	int nevt = c.GetEntries();
+	cout<<nevt<<" events to be processed"<<endl;
+	for (int i=0; i<nevt; i++) {
+		if (i%10000==0) cout<<"Processing event "<<i<<endl;
 
-	cout<<told->GetEntries()<<" events to be processed"<<endl;
-	for (int i=0; i<told->GetEntries(); i++) {
-		if (i%5000==0) cout<<"Processing event "<<i<<endl;
+		c.GetEntry(i);
+		if (dp>0.9||f<0.35||f>0.5||dt<80||dt>100||e>1e5) continue;
 
-		// process CsI waveform
-		told->GetEntry(i);
-		m=1250;
-		if (ht<325||ht>600) continue;
-		if (hm>500) continue;
-		pc=0; pbc=0; dpc=0; dwi=0;
-		for (int k=200; k<200+100; k++)	pc+=vc[k]; pc/=100;
-			for (int k=750; k<1000; k++)	pbc+=vc[k]; pbc/=250;
-			for (int k=0; k<m; k++) {
-				if (k<300) dpc+=(vc[k]-pc)*(vc[k]-pc); // calculate pedestal RMS
-				if (k>300&&k<1000) dwi+=(vc[k]-pc)*(vc[k]-pc);//trigger range RMS
-				vc[k]-=pc; // remove pedestal
-			}
-		dwi=sqrt(dwi)/700;
-		dpc=sqrt(dpc)/300; // RMS of pedestal
+		e3=e4=e5=rms=0; // calculate new parameters
+		for (int k=1600/4; k<5000/4; k++) {
+			if (k<3000/4) { e3+=v[k]; rms+=v[k]*v[k]; }
+			if (k<4000/4) { e4+=v[k]; }
+			e5+=v[k];
+		}
+		rms=sqrt(rms)/350;
 
-		if (pc<-10||pc>10) continue;
-		if (dpc>0.6) continue;
-		if ((pbc-pc)>5||(pbc-pc)<-5) continue;
 		t->Fill();
 	}
 	t->Write("",TObject::kOverwrite);
-	output->Close();
-	gSystem->Chmod(output->GetName(),S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
-	cout<<"File "<<output->GetName()<<" saved"<<endl;
+	output.Close();
+	gSystem->Chmod(output.GetName(),S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
+	cout<<"File "<<output.GetName()<<" saved"<<endl;
 }
